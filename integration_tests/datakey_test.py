b6cd92f4b9991ba0732ff12d4bb1aa34ecb48a4d
usmaanwaheed <usmaan.waheed@dwp.gsi.gov.uk>
2019-06-26 10:05:48 +0100

    data_key integration tests 2

--- integration_tests/datakey_test.py
+++ integration_tests/datakey_test.py
@@ -1,15 +1,28 @@
-
 from assertpy import assert_that
 import requests
 
 
-def get_new_datakey():
+def test_get_new_data_key():
     response = requests.get("http://localhost:8080/datakey")
     response.raise_for_status()
     return response.json()
 
 
-def decrypt_datakey(key_id, encrypted_key):
+def test_get_ping():
+    response = requests.get("http://localhost:8080/ping")
+    response.raise_for_status()
+    return response.json()
+
+
+def test_health_check():
+    response = requests.get("http://localhost:8080/healthcheck")
+    response.raise_for_status()
+    assert_that(test_health_check()).contains_only({'encryptionService': 'OK', 'masterKey': 'OK',
+                                                   'dataKeyGenerator': 'OK', 'encryption': 'OK', 'decryption': 'OK'})
+    return response.json()
+
+
+def test_decrypt_data_key(key_id, encrypted_key):
     response = requests.request(
         "POST",
         "http://localhost:8080/datakey/decrypt",
@@ -19,19 +32,21 @@ def decrypt_datakey(key_id, encrypted_key):
     return response.text()
 
 
-def datakey_returns_valid_datakey_pair():
-    # Given a new datakey is generated
-    data_key = get_new_datakey()
+def test_data_key_returns_valid_data_key_pair():
+    # Given a new data_key is generated
+    data_key = test_get_new_data_key()
 
-    # When the datakey is decrypted
-    decrypted_key = decrypt_datakey(
+    # When the data_key is decrypted
+    decrypted_key = test_decrypt_data_key(
         data_key.get("dataKeyEncryptionKeyId"),
         data_key.get("ciphertextDataKey"))
 
-    # Then the datakey has all the required values
+    # Then the data_key has all the required values
     assert_that(data_key.keys()).contains_only("dataKeyEncryptionKeyId", "plaintextDataKey", "ciphertextDataKey")
 
     # And the decrypted key matches the plaintext key
     assert_that(decrypted_key).is_equal_to(data_key.get("plaintextDataKey"))
+
+
 
 
