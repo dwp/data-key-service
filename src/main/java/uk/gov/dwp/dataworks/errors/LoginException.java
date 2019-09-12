be85ea2f00f87fa178d876bcd43a41243ef7c9e8
Daniel Chicot <dan.chicot@gmail.com>
2019-09-12 19:13:40 +0100

    LoginException gives service unavailable.

--- src/main/java/uk/gov/dwp/dataworks/errors/LoginException.java
+++ src/main/java/uk/gov/dwp/dataworks/errors/LoginException.java
@@ -3,7 +3,7 @@ package uk.gov.dwp.dataworks.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
 public class LoginException extends RuntimeException {
     public LoginException(Throwable e) {
         super(e);
