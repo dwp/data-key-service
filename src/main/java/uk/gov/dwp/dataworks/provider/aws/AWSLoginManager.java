a395faba3820b606270f129c0dd0eaa94788b071
Daniel Chicot <dan.chicot@gmail.com>
2019-09-12 13:41:46 +0100

    Optimized imports.

--- src/main/java/uk/gov/dwp/dataworks/provider/aws/AWSLoginManager.java
+++ src/main/java/uk/gov/dwp/dataworks/provider/aws/AWSLoginManager.java
@@ -1,9 +1,6 @@
 package uk.gov.dwp.dataworks.provider.aws;
 
-import uk.gov.dwp.dataworks.errors.LoginException;
-
 public interface AWSLoginManager {
-
-    void login() throws LoginException;
-    void logout() throws LoginException;
+    void login();
+    void logout();
 }
