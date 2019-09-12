08868df59adf5be7251560a5747ad7971d4ceda8
Daniel Chicot <dan.chicot@gmail.com>
2019-09-12 13:40:37 +0100

    Optimized imports.

--- src/main/java/uk/gov/dwp/dataworks/provider/DataKeyDecryptionProvider.java
+++ src/main/java/uk/gov/dwp/dataworks/provider/DataKeyDecryptionProvider.java
@@ -1,7 +1,6 @@
 package uk.gov.dwp.dataworks.provider;
 
 import uk.gov.dwp.dataworks.dto.DecryptDataKeyResponse;
-import uk.gov.dwp.dataworks.errors.LoginException;
 
 public interface DataKeyDecryptionProvider extends Dependent {
     int MAX_PAYLOAD_SIZE = 32000;
--- src/main/java/uk/gov/dwp/dataworks/provider/DataKeyGeneratorProvider.java
+++ src/main/java/uk/gov/dwp/dataworks/provider/DataKeyGeneratorProvider.java
@@ -2,7 +2,6 @@ package uk.gov.dwp.dataworks.provider;
 
 import uk.gov.dwp.dataworks.dto.GenerateDataKeyResponse;
 import uk.gov.dwp.dataworks.errors.DataKeyGenerationException;
-import uk.gov.dwp.dataworks.errors.LoginException;
 
 public interface DataKeyGeneratorProvider extends Dependent {
     GenerateDataKeyResponse generateDataKey(String encryptionKeyId) throws DataKeyGenerationException;
