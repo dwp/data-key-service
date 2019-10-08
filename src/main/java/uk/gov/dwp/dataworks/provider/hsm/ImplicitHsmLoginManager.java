8d9acf8cff509ba8f56a86ec0e07a7f9cf866c27
Daniel Chicot <dan.chicot@gmail.com>
2019-10-08 20:39:44 +0100

    Delete unwrapped (#45)
    * Explicit log out.
    
    * Delete unwrapped key.

--- src/main/java/uk/gov/dwp/dataworks/provider/hsm/ImplicitHsmLoginManager.java
+++ src/main/java/uk/gov/dwp/dataworks/provider/hsm/ImplicitHsmLoginManager.java
@@ -1,16 +1,11 @@
 package uk.gov.dwp.dataworks.provider.hsm;
 
-import com.cavium.cfm2.CFM2Exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import uk.gov.dwp.dataworks.dto.HSMCredentials;
-import uk.gov.dwp.dataworks.errors.LoginException;
-import uk.gov.dwp.dataworks.errors.MasterKeystoreException;
 import uk.gov.dwp.dataworks.provider.HsmLoginManager;
 
 @Component
@@ -35,22 +30,8 @@ public class ImplicitHsmLoginManager implements HsmLoginManager {
         }
     }
 
-    @Override
-    @Retryable(
-            value = { MasterKeystoreException.class },
-            maxAttempts = 10,
-            backoff = @Backoff(delay = 1_000))
-    public  void logout() throws LoginException, MasterKeystoreException {
-        try {
-            LOGGER.info("Explicitly logging out.");
-            loginManager.logout();
-        }
-        catch (CFM2Exception e) {
-            throw new MasterKeystoreException();
-        }
-    }
 
-//    @Override
-//    public  void logout() {
-//    }
+    @Override
+    public  void logout() {
+    }
 }
