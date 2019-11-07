8f05bf63b4775e49aa074fb929610cadf78a3ca3
Daniel Chicot <dan.chicot@gmail.com>
2019-11-07 11:23:22 +0000

    Remove stderr.

--- src/main/java/uk/gov/dwp/dataworks/controller/HealthCheckController.java
+++ src/main/java/uk/gov/dwp/dataworks/controller/HealthCheckController.java
@@ -89,7 +89,6 @@ public class HealthCheckController {
             canEncryptDataKey = ! Strings.isNullOrEmpty(encryptResponse.ciphertextDataKey);
             DecryptDataKeyResponse decryptResponse =
                     dataKeyService.decrypt(encryptResponse.dataKeyEncryptionKeyId, encryptResponse.ciphertextDataKey);
-            System.err.println(decryptResponse);
             canDecryptDataKey = ! Strings.isNullOrEmpty(decryptResponse.plaintextDataKey) &&
                     decryptResponse.plaintextDataKey.equals(encryptResponse.plaintextDataKey);
         }
