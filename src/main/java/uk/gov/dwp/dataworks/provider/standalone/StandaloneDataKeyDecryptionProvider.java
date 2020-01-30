25bf63e8bd091d7189a543ce4cb741651c4c66d9
Mark Matthews <mark.matthews@infinityworks.com>
2020-01-30 16:36:45 +0000

    Add debug logs for developer mode testing (#62)

--- src/main/java/uk/gov/dwp/dataworks/provider/standalone/StandaloneDataKeyDecryptionProvider.java
+++ src/main/java/uk/gov/dwp/dataworks/provider/standalone/StandaloneDataKeyDecryptionProvider.java
@@ -1,10 +1,13 @@
 package uk.gov.dwp.dataworks.provider.standalone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import uk.gov.dwp.dataworks.dto.DecryptDataKeyResponse;
 import uk.gov.dwp.dataworks.provider.DataKeyDecryptionProvider;
+import uk.gov.dwp.dataworks.provider.hsm.EncryptingCaviumCryptoImplementationSupplier;
 import uk.gov.dwp.dataworks.util.ArrayUtils;
 
 import java.nio.ByteBuffer;
@@ -23,16 +26,21 @@ public class StandaloneDataKeyDecryptionProvider implements DataKeyDecryptionPro
 
     @Override
     public DecryptDataKeyResponse decryptDataKey(String dataKeyEncryptionKeyId, String ciphertextDataKey, String correlationId) {
+        LOGGER.debug("decryptDataKey: dataKeyEncryptionKeyId: '{}', ciphertextDataKey: '{}', correlation_id: {}", dataKeyEncryptionKeyId, ciphertextDataKey, correlationId);
         ByteBuffer ciphertextDataKeyBuffer = ByteBuffer.wrap(decoder.decode(ciphertextDataKey));
         byte[] decrypted = ciphertextDataKeyBuffer.array();
+        // reverse the bytes
         ArrayUtils.reverse(decrypted);
         String plaintext = encoder.encodeToString(decrypted);
-        return new DecryptDataKeyResponse(dataKeyEncryptionKeyId,
-                plaintext);
+        DecryptDataKeyResponse response = new DecryptDataKeyResponse(dataKeyEncryptionKeyId, plaintext);
+        LOGGER.debug("decryptDataKey: response.hashcode(): '{}', correlation_id: {}", response.hashCode(), correlationId);
+        return response;
     }
 
     @Override
     public boolean canSeeDependencies() {
         return true;
     }
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(StandaloneDataKeyDecryptionProvider.class);
 }
--- src/main/java/uk/gov/dwp/dataworks/provider/standalone/StandaloneDataKeyGeneratorProvider.java
+++ src/main/java/uk/gov/dwp/dataworks/provider/standalone/StandaloneDataKeyGeneratorProvider.java
@@ -1,5 +1,7 @@
 package uk.gov.dwp.dataworks.provider.standalone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -22,27 +24,24 @@ public class StandaloneDataKeyGeneratorProvider implements DataKeyGeneratorProvi
 
     @Override
     public GenerateDataKeyResponse generateDataKey(String keyId, String correlationId) {
-
+        LOGGER.debug("generateDataKey: keyId: '{}', correlation_id: {}", keyId, correlationId);
         // Generate a random key
         int keySize = 128 / 8;
         byte[] key = new byte[keySize];
         new Random().nextBytes(key);
         String plaintextKey = encoder.encodeToString(key);
-
         // reverse the bytes
         ArrayUtils.reverse(key);
         String encryptedKey = encoder.encodeToString(key);
-
-        return new GenerateDataKeyResponse(
-                keyId,
-                plaintextKey,
-                encryptedKey
-        );
-
+        GenerateDataKeyResponse response = new GenerateDataKeyResponse(keyId, plaintextKey, encryptedKey);
+        LOGGER.debug("generateDataKey: response.hashCode(): '{}', correlation_id: {}", response.hashCode(), correlationId);
+        return response;
     }
 
     @Override
     public boolean canSeeDependencies() {
         return true;
     }
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(StandaloneDataKeyGeneratorProvider.class);
 }
